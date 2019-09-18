e6cff1d91bf47b6e737770a119d3eb5e339dc86c
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-09-18 18:59:16 -0400

    New production build

--- frontend/src/app/sub2/sub2.module.ts
+++ frontend/src/app/sub2/sub2.module.ts
@@ -5,9 +5,10 @@ import { Comp1Component } from "./comp1/comp1.component";
 import { Comp2Component } from "./comp2/comp2.component";
 import { IndexComponent } from "./index/index.component";
 import { Sub2RoutingModule } from "./sub2-routing.module";
+import { Sub2Component } from "./sub2.component";
 
 @NgModule({
-  declarations: [Comp1Component, Comp2Component, IndexComponent],
+  declarations: [Comp1Component, Comp2Component, IndexComponent, Sub2Component],
   imports: [CommonModule, Sub2RoutingModule],
   providers: []
 })
