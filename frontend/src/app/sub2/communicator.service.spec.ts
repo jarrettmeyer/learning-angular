0e7b4851d26bc42746e12f83cb583b9ba789371a
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-09-18 14:48:10 -0400

    Adds unit tests for communicator service

--- frontend/src/app/sub2/communicator.service.spec.ts
+++ frontend/src/app/sub2/communicator.service.spec.ts
@@ -1,12 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { CommunicatorService } from './communicator.service';
+import { CommunicatorService } from "./communicator.service";
 
-describe('CommunicatorService', () => {
+describe("CommunicatorService", () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it("should be created", () => {
     const service: CommunicatorService = TestBed.get(CommunicatorService);
     expect(service).toBeTruthy();
+  });
+
+  it("has an initial value of 0", () => {
+    const service: CommunicatorService = TestBed.get(CommunicatorService);
+    expect(service.clickCount).toEqual(0);
+  });
+
+  it("can increment value", () => {
+    const service: CommunicatorService = TestBed.get(CommunicatorService);
+    service.increment();
+    service.increment();
+    service.increment();
+    expect(service.clickCount).toEqual(3);
+  });
+
+  it("can reset value", () => {
+    const service: CommunicatorService = TestBed.get(CommunicatorService);
+    service.increment();
+    service.increment();
+    service.increment();
+    expect(service.clickCount).toEqual(3);
+    service.reset();
+    expect(service.clickCount).toEqual(0);
   });
 });
